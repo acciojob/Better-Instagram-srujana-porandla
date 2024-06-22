7f6ad4d1de4a596776cab8016694ad1ca6cf1994
srujana-porandla <131435996+srujana-porandla@users.noreply.github.com>
2024-06-22 18:24:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your code here
+document.addEventListener('DOMContentLoaded', (event) => {
+    let dragged;
+
+    document.querySelectorAll('.image').forEach(item => {
+        item.addEventListener('dragstart', (e) => {
+            dragged = e.target;
+            e.dataTransfer.setData('text/plain', e.target.id);
+        });
+
+        item.addEventListener('dragover', (e) => {
+            e.preventDefault();
+        });
+
+        item.addEventListener('drop', (e) => {
+            e.preventDefault();
+            if (dragged !== e.target) {
+                let draggedBackground = dragged.style.backgroundImage;
+                let targetBackground = e.target.style.backgroundImage;
+                dragged.style.backgroundImage = targetBackground;
+                e.target.style.backgroundImage = draggedBackground;
+            }
+        });
+    });
+});
+
+
